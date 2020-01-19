3acd87876aee5b934f6048026ff70e0d2b3b17f1
tuberculo <maltacastro@gmail.com>
2020-01-19 19:46:13 +0100

    Inclusão do arquivo vazões do PDE 2029.

--- calc_incr.R
+++ calc_incr.R
@@ -4,7 +4,8 @@ cascata_PDE_2029 <- read_delim("cascata - PDE 2029.csv", ";", escape_double = FA
 cascata_PDE_2022 <- read_delim("cascata - PDE 2022.csv", ";", escape_double = FALSE, locale = locale(date_names = "pt", decimal_mark = ",", grouping_mark = "."), trim_ws = TRUE)
 
 #Vazoes2022 <- read_fwf("vazao-pde2022.txt", fwf_empty("vazao-pde2022.txt", col_names = c("Posto", "Ano", "janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro")), col_types = cols(.default = "d"))
-ArquivoVazoes <- "vazao-pde2022.txt" # Arquivo vazões no formato Newave
+ArquivoVazoes2022 <- "vazao-pde2022.txt" # Arquivo vazões no formato Newave
+ArquivoVazoes2029 <- "vazoes2029.txt"
 
 CalcIncr <- function(ArquivoVazoes, Cascata){
   Vazoes <- read_fwf(ArquivoVazoes, fwf_empty(ArquivoVazoes, col_names = c("Posto", "Ano", 1:12), n = 10000L), col_types = cols(.default = "d"))
@@ -12,10 +13,10 @@ CalcIncr <- function(ArquivoVazoes, Cascata){
   VazJoin <- left_join(Vazoes, Cascata, by = c("Posto" = "posto"))
   
   VazComp <- Vazoes
-  for (j in 1:6){
+  for (j in 1:6) {
     VazComp <- left_join(Vazoes, select(VazJoin, Ano, Mes, Posto, paste0("Posto montante ", j)), by = c("Posto" = paste0("Posto montante ", j), "Mes" = "Mes", "Ano" = "Ano")) %>% right_join(VazComp, by = c("Posto.y" = "Posto", "Mes" = "Mes", "Ano" = "Ano"))
-    colnames(VazComp)[colnames(VazComp)==c("Posto", "Vazao.x")] <- c(paste0("PostoMont",j), paste0("VazaoMont",j))
-    colnames(VazComp)[colnames(VazComp)==c("Posto.y", "Vazao.y")] <- c("Posto", "Vazao")
+    colnames(VazComp)[colnames(VazComp) == c("Posto", "Vazao.x")] <- c(paste0("PostoMont",j), paste0("VazaoMont",j))
+    colnames(VazComp)[colnames(VazComp) == c("Posto.y", "Vazao.y")] <- c("Posto", "Vazao")
     #VazComp2022 <- left_join(VazComp2022, select(VazJoin2022, Ano, Mês, Posto, paste0("Posto montante ", j)), by = c("Posto" = paste0("Posto montante ", j), "Mês" = "Mês", "Ano" = "Ano"), suffix=c(".x",paste0(".Jus", j)))
   }
   
@@ -38,7 +39,7 @@ filter(vaz2022, Posto == 2) %>% mutate(VazMontTV1 = lag(VazaoMont1, 5))
 # Falta fazer: Para outros montantes (2 a 6). Deixar como função. Retirar coluna TempViagem1 do tibble de vazões.
 
 temp1 <- NULL
-for (p in unique(Testevaz2022$Posto)){
+for (p in unique(Testevaz2022$Posto)) {
   temp <- mutate(filter(Testevaz2022, Posto == p), VazaoMontTV1 = lag(VazaoMont1, filter(TempoViagem1, Posto == p)[[2]]))
   temp1 <- bind_rows(temp1, temp)
 }
