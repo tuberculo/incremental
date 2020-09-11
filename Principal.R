1aa8e47ddc3444deb54041f6adb843063e85dcc1
Gabriel <maltacastro@gmail.com>
2020-09-11 18:56:52 -0300

    Atualização de nomes de reservatórios.

--- Principal.R
+++ Principal.R
@@ -13,7 +13,7 @@ if (!require(tidyverse)) {
 
 
 # Dados -------------------------------------------------------------------
-ArquivoVazoesDiarias <- "../Vazões_Diarias_ajuste_mensal_1982_nat+art_ONS+Exp_r06.csv"
+ArquivoVazoesDiarias <- "../Vazões_Diarias_ajuste_mensal_1982_nat+art_ONS+Exp_r09.csv"
 if (length(commandArgs(trailingOnly = TRUE)) != 0) ArquivoVazoesDiarias <- commandArgs(trailingOnly = TRUE)
 ArqTV <- "P_tempo de viagem.xlsx"
 ArqNomesReservat <- "NomesReservat.csv"
